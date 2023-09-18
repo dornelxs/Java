7b1394a981f060717d2de3f004f51c5bfbdf928f
dornelxs <dornelaschris@gmail.com>
2023-09-18 19:13:20 -0300

    nota_de_aula3

---
+++ Aula5/src/Questao2/src/Diretor.java
@@ -0,0 +1,25 @@
+public class Diretor extends Funcionario {
+    private int acoes;
+
+    public Diretor(String nome, double salario, int acoes, String funcao) {
+        super(nome, salario, funcao);
+        this.acoes = acoes;
+    }
+
+    public void setAcoes(int acoes) {
+        this.acoes = acoes;
+    }
+
+    public int getAcoes() {
+        return acoes;
+    }
+
+    @Override
+    public String toString() {
+        return "|-----------------FUNCIONÁRIO-----------------|" + "\n" +
+                "|Nome: " + getNome() + "|" + "\n" +
+                "|Função: "+ getFuncao() + "|" + "\n" +
+                "|Salário: "+ getSalario() + "|" + "\n" +
+                "|Quantidade de ações: " + acoes + "|";
+    }
+}
